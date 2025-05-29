ad96bc138beb1a9a32f235f1463dc05b5d249d4c
Tanizak Soma <xndu09@gmail.com>
2025-05-30 00:00:45 +0900

    saving

--- list.h
+++ list.h
@@ -13,14 +13,14 @@
 # define ERROR -1
 # define END_OF_LIST -2
 
-typedef struct t_node
+typedef struct s_node
 {
 	char			data;
 	struct t_node	*next;
 }	t_node;
 
-void cleanup(t_node *head);
-t_node *append(t_node *head, char data);
-char *list_tostr(t_node *head);
+void	cleanup(t_node *head);
+t_node	*append(t_node *head, char data);
+char	*list_tostr(t_node *head);
 
 #endif
