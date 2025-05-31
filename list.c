85f7a0fe90443aa3d966e84e479c666ef5f9bb7b
Tanizak Soma <xndu09@gmail.com>
2025-05-31 22:58:07 +0900

    saving the formatted version of list repository

--- list.c
+++ list.c
@@ -1,8 +1,6 @@
 #include "list.h"
 
-extern int	allocation_counter;
-
-void cleanup(t_node *head)
+void	cleanup(t_node *head)
 {
 	t_node	*current;
 	t_node	*next;
@@ -10,41 +8,35 @@ void cleanup(t_node *head)
 	current = head;
 	while (current)
 	{
-        next = current->next;
-        free(current);
-        current = next;
-        allocation_counter--;
-    }
+		next = current->next;
+		free(current);
+		current = next;
+	}
 }
 
-t_node *append(t_node *head, char data)
+t_node	*append(t_node *head, char data)
 {
-    t_node *new;
-    t_node *current;
+	t_node	*new;
+	t_node	*current;
 
-    new = malloc(sizeof(t_node));
-    if (!new)
+	new = malloc(sizeof(t_node));
+	if (!new)
 	{
-        cleanup(head);
-        return (NULL);
-    }
-
-    allocation_counter++;
-    new->data = data;
-    new->next = NULL;
-    
-    if (!head)
-        return (new);
-    
-    current = head;
-    while (current->next)
-        current = current->next;
-    
-    current->next = new;
-    return (head);
+		cleanup(head);
+		return (NULL);
+	}
+	new->data = data;
+	new->next = next;
+	if (head == NULL)
+		return (NULL);
+	current = head;
+	while (current->next)
+		current = current->next;
+	current->next = new;
+	return (head);
 }
 
-size_t	list_len(const t_node	*head)
+size_t	list_len(const t_node *head)
 {
 	size_t			length;
 	const t_node	*current;
@@ -59,27 +51,25 @@ size_t	list_len(const t_node	*head)
 	return (length);
 }
 
-//list_tostr does not rewrite the data in the list. Therefore we must give the constant.
-char *list_tostr(const t_node *head)
+char	*list_tostr(const t_node *head)
 {
-    char	*line;
-    t_node	*current;
-    size_t	length;
-    size_t	i;
+	char	*line;
+	t_node	*current;
+	size_t	length;
+	size_t	i;
 
-	length = list_len((const t_node *)head);
-    line = malloc(sizeof(char) * (length + 1));
-    if (!line)
-        return NULL;
-    allocation_counter++;
-    current = (t_node *)head;
-    i = 0;
-    while (current)
+	length = list_len(head);
+	line = malloc(sizeof(char) * (length + 1));
+	if (!line)
+		return (NULL);
+	current = (t_node *)head;
+	i = 0;
+	while (current)
 	{
-        line[i] = current->data;
-        current = current->next;
-        i++;
-    }
-    line[i] = '\0';
-    return line;
+		line[i] = current->data;
+		current = current->next;
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
 }
